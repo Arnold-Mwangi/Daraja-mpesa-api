9f03e79ae9371951ecee829b94da0fd5f5e30cd9
Kirigwi <kirigwimwangi18@yahoo.com>
2022-05-05 14:27:08 +0300

    second commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,9 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'iA7Lt0ERZ3RyDJ6zzxq6PM54a607G3sn
+  '; //Fill with your app Consumer Key
+  $consumerSecret = 'GidvjL6cP0GXklYR'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
@@ -42,7 +43,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://morning-basin-87523.herokuapp.com/callback_url.php';  
+  $CallBackURL = 'https://stark-fjord-61035.herokuapp.com/callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
